bdd9ac79ecc96a737b23fd82bfb182dd3dbeebc9
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-04 18:23:54 +0530

    adding db to fetch data from db

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
+// export async function GET(request){
+//     return NextResponse.json({name:"Ritik",age:24,city:"mumbai"},{status:200})
+// }
 
-export async function GET(request){
-    return NextResponse.json({name:"Ritik",age:24,city:"mumbai"},{status:200})
+// ------------------------------------------------------------------
+import {user} from '@/util/db'
+export function GET(request){
+    const data = user;
+    return NextResponse.json(data, {status:200});
 }
---
+++ src/util/db.js
@@ -0,0 +1,22 @@
+export const user = [
+    {
+        name: 'John',
+        age: 36,
+        email: 'john@example.com'
+    },
+    {
+        name: 'peter',
+        age: 26,
+        email: 'peter@example.com'
+    },
+    {
+        name: 'roy',
+        age: 56,
+        email: 'roy@example.com'
+    },
+    {
+        name: 'sam',
+        age: 24,
+        email: 'sam@example.com'
+    }
+]
