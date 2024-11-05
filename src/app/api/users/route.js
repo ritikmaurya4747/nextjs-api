d9bb6967e9bd2990d3984923f2b9671d74b0c1ae
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-11-05 12:09:43 +0530

    tested dynamic post api

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -10,3 +10,12 @@ export function GET(req,content){
     const data = user;
     return NextResponse.json(data, {status:200});
 }
+export async function POST(req,res){
+    const payload = await req.json();
+    // console.log(payload);
+    // console.log(payload.name);
+    if(!payload.name||!payload.age||!payload.email){
+        return NextResponse.json({message:"all fields are required",success:false},{status:404})
+    }
+    return NextResponse.json({message:"New user created" ,success:true},{status:200})
+}
